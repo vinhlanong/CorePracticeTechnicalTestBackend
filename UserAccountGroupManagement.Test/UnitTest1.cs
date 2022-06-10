baf94c6846e7cda8f7764e4cec5282f4f3868152
vinhlanong <vinhlan.ong@gmail.com>
2022-06-10 15:19:08 +1000

    unit testing

--- UserAccountGroupManagement.Test/UnitTest1.cs
+++ UserAccountGroupManagement.Test/UnitTest1.cs
@@ -9,6 +9,8 @@ namespace UserAccountGroupManagement.Test
         [TestMethod]
         public void TestMethod1()
         {
+            bool result = false;
+            Assert.AreEqual(false, result);
         }
     }
 }
